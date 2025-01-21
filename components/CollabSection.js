6e98e428daf2d2e356ca0620df696f8216c2ae6c
KuzahmetovaAdelya <137976799+KuzahmetovaAdelya@users.noreply.github.com>
2025-01-21 14:21:50 +0500

    v.7

--- components/CollabSection.js
+++ components/CollabSection.js
@@ -4,7 +4,8 @@ export default function CollabSection() {
             <div className="w-350 mx-auto mt-20 lg:w-tablet lg:mt-40 lg:mb-20 xl:w-notepad xl:mt-64 2xl:w-desktop">
                 <div className="lg:flex lg:justify-between lg:items-center lg:mb-10 xl:mb-5">
                     <p className="text-white font-medium text-lg w-80 tracking-tighter hidden lg:block">Наш бренд всегда открыт к сотрудничеству и классным коллабам</p>
-                    <h2 className="uppercase text-white text-2xl font-semibold tracking-tighter lg:text-p-lg xl:text-number-lg leading-tight">Коллаборации</h2>
+                    <h2 className="hidden lg:block uppercase text-white text-2xl font-semibold tracking-tighter lg:text-p-lg xl:text-number-lg leading-tight">Коллаборации</h2>
+                    <h2 className="uppercase text-white text-2xl font-semibold tracking-tighter mb-5 lg:hidden">Коллаборации</h2>
                 </div>
                 <div className="flex justify-between items-start mb-6 lg:hidden">
                     <p className="text-white font-medium text-p w-52 tracking-tighter">Наш бренд всегда открыт к сотрудничеству и классным коллабам</p>
@@ -12,15 +13,18 @@ export default function CollabSection() {
                         <div className="border-white border-2 rounded-full p-2.5"><img src="/arrow.svg" className="w-19 h-19"></img></div>
                     </button>
                 </div>
-                <div className="flex justify-between">
+
+
+                
+                <div className="hidden lg:flex justify-between">
                     <div>
                         <img src="/photo.png" className="w-350 lg:w-30 lg:h-60 xl:w-870 xl:h-600 object-contain"></img>
                         <p className="text-white font-medium text-p tracking-tighter text-center opacity-60 lg:text-left lg:text-lg lg:mt-2.5">Музыкант / DJ / Biicla</p>
                     </div>
                     
-                    <div className="w-424 lg:mt-0 xl:mt-10 flex flex-col justify-between">
+                    <div className="w-270 xl:w-424 lg:mt-0 xl:mt-10 flex flex-col justify-between">
                         <div>
-                            <p className="text-white font-medium text-lg tracking-tighter">Продюсер, диджей и автор песен Виталий Жариков (Biicla) попал в лонг-лист рейтинга «З0 до 30», который ежегодно составляет российский Forbes. В этот список попадают те, кто к 30 годам получили признание в профессиональном сообществе и стали известны на всероссийском или даже глобальном уровне. Сейчас стартовало голосование в каждой из 10 номинаций. Biicla, как нетрудно догадаться, борется за победу в категории «Музыка».</p>
+                            <p className="text-white font-medium lg:text-p lg:leading-5 xl:leading-7 xl:text-lg tracking-tighter">Продюсер, диджей и автор песен Виталий Жариков (Biicla) попал в лонг-лист рейтинга «З0 до 30», который ежегодно составляет российский Forbes. В этот список попадают те, кто к 30 годам получили признание в профессиональном сообществе и стали известны на всероссийском или даже глобальном уровне. Сейчас стартовало голосование в каждой из 10 номинаций. Biicla, как нетрудно догадаться, борется за победу в категории «Музыка».</p>
                             <p className="hidden xl:block text-white font-medium text-p tracking-tighter text-center opacity-60 lg:text-left lg:text-lg lg:mt-10">Музыкант / DJ / Biicla</p>
                         </div>
 
@@ -31,7 +35,26 @@ export default function CollabSection() {
                             <a href="" className="group"><img src="/utube.svg" className="group-hover:opacity-40 w-10 lg:w-11 lg:h-11 xl:w-11 2xl:w-12"></img></a>
                         </div>
                     </div>
+                </div>
 
+                <div className="lg:hidden">
+                    <div>
+                        <img src="/photo.png" className="w-350 mb-3"></img>
+                        <div>
+                            <div className="flex w-screen gap-2.5 overflow-hidden mb-1 absolute left-0">
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                                <h3 className="text-red font-medium tracking-tighter text-4xl">BICLA</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p className="text-white font-medium text-p tracking-tighter text-center mt-75">Музыкант / DJ / Biicla</p>
                 </div>
 
             </div>
